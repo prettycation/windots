0699c6e470a052b583728394dadd8daa8714a611
prettycation <a2095578831@gmail.com>
2026-02-04 23:45:34 +0800

    BREAKING CHANGE: 改用lazyvim

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,7 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.set("i", "jk", "<Esc>", { desc = "退出插入模式", silent = true })
+
+vim.keymap.set("c", "jk", "<Esc>", { desc = "退出命令模式", silent = true })
--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -1,16 +1,27 @@
--- 文件路径: ~/AppData/Local/nvim/lua/plugins/flash.lua
-
 return {
-    "folke/flash.nvim",
-    event = "VeryLazy",
-    
-    -- [核心修改] 通过 'keys' 选项覆盖默认快捷键
-    keys = {
-      -- 将 'f' 键映射为全局跳转 (原本 's' 的功能)
-      { "f", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash Global Jump" },
-      
-      -- 为 'F' 键映射一个更强大的 Treesitter 跳转，它能识别代码结构
-      { "F", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-      
+  "folke/flash.nvim",
+
+  opts = {
+    modes = {
+      char = {
+        enabled = false, -- 禁用 flash.nvim 的字符搜索模式
+      },
     },
+  },
+
+  keys = {
+    -- 禁用默认的 's' 键
+    { "s", mode = { "n", "x", "o" }, false },
+    { "S", mode = { "n", "x", "o" }, false },
+
+    -- 将 'f' 映射到 jump 功能
+    {
+      "f",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").jump()
+      end,
+      desc = "Flash Jump",
+    },
+  },
 }
